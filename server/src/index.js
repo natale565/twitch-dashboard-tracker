4c8a3e889739b03a14c174eb077069b795bd8641
natale565 <natale565@icloud.com>
2026-02-16 10:32:31 -0500

    db-test created

--- server/src/index.js
+++ server/src/index.js
@@ -9,6 +9,18 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+app.get('/db-test', async function(req, res) {
+    try {
+        const result = await pool.query('SELECT NOW()')
+        res.send(result.rows[0])
+    }
+    catch(error){
+        console.log('database failed', error)
+        res.status(500).send('database failed');
+    }
+    
+})
+
 const app = express();
 app.get('/health', function(req, res){res.send({ok : true})} );
 app.listen(3001);
